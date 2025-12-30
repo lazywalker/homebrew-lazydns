1d2841cfddd01bd14feb9690a3c7897c97911d3f
lazywalker <lazywalkerz@gmail.com>
2025-12-30 08:41:10 +0800

    Update lazydns to version 0.2.52

--- Formula/lazydns.rb
+++ Formula/lazydns.rb
@@ -1,27 +1,27 @@
 class Lazydns < Formula
   desc "A DNS server/forwarder implementation in Rust, inspired by mosdns"
   homepage "https://github.com/lazywalker/lazydns"
-  version "0.2.51"
+  version "0.2.52"
   license "GPL-3.0-or-later"
 
   on_arm do
     url "https://github.com/lazywalker/lazydns/releases/download/v#{version}/lazydns-full-aarch64-apple-darwin.tar.gz"
-    sha256 "de30c10dd089fc62893f067472bb6a45215b097b39ae44b5d2e8adcfbbfe64e5"
+    sha256 "e487b20f1eda80229f0f3b2fb47d47e02c62f18874f2379c1de0b7081685aa92"
   end
 
   on_intel do
     url "https://github.com/lazywalker/lazydns/releases/download/v#{version}/lazydns-full-x86_64-apple-darwin.tar.gz"
-    sha256 "de43663e789bcb1f2fe7d7a92a3d729bde41bc392b355fa1e55e7eaef2a89f46"
+    sha256 "a5af4764acd57ca4decad90bc1c4b2cc1e03fabd72694f724022a885125f79d7"
   end
 
   on_linux do
     on_intel do
       url "https://github.com/lazywalker/lazydns/releases/download/v#{version}/lazydns-full-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "5453da55d50cae0538df5cf8ca97c7cea0a8851a46fc45a4c364847e526ad543"
+      sha256 "38c5427d19ab64d0e7f19d8768b7a1939e72f9144a96f1c9dc74cf1d9a69c680"
     end
     on_arm do
       url "https://github.com/lazywalker/lazydns/releases/download/v#{version}/lazydns-full-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "c822226cfa727e6d5da95b51817923bbe62fbb84949cf73bda3e5c64bf75fd6b"
+      sha256 "e6627e5d55012338c9ca05fe1df69e977cfaba09d29a80d71d8eb18e3961003f"
     end
   end
 
